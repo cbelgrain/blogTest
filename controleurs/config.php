7cc48c2e9b95022ec56c42c463fb912ab71d2f2c
cbelgrain <cecilia.belgrain@gmail.com>
2019-06-14 15:42:03 +0200

    nouveau logo

--- controleurs/config.php
+++ controleurs/config.php
@@ -2,7 +2,7 @@
 
 	//info site
 	$titreSite = "mon blog";
-	$logo = "<img class='logo' src= 'img/logo.png' alt='logo' />";
+	$logo = "<img class='logo' src= 'img/logo2.png' alt='logo' />";
 
 	// identifiant bdd
 	$host = "mysql:host=localhost;dbname=blog_eb";
